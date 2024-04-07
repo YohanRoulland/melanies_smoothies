a8aa86486ad384d829c31075e6ed0c3b0aa7419d
Yohan+ <yohan.roulland@gmail.com>
2024-04-07 16:12:10 +0200

    correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,6 @@ session = cnx.session()
 my_dataframe = session.table(
     "smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 pd_df = my_dataframe.to_pandas()
-st.write(pd_df)
 
 name_on_order = st.text_input("Choose your name:")
 st.write('The name on your Smoothie will be:', name_on_order)
