20d1f0200ef77545e2c127be344592850734e7f8
Yohan+ <yohan.roulland@gmail.com>
2024-04-07 16:09:13 +0200

    info nutri

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ session = cnx.session()
 my_dataframe = session.table(
     "smoothies.public.fruit_options").select(col('SEARCH_ON'))
 pd_df = my_dataframe.to_pandas()
+st.write(pd_df)
 
 name_on_order = st.text_input("Choose your name:")
 st.write('The name on your Smoothie will be:', name_on_order)
