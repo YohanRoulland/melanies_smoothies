df77dc28d1f8eb4eb024aadf0bb5763a25d43743
Yohan+ <yohan.roulland@gmail.com>
2024-04-07 16:06:35 +0200

    info nutri

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import pandas as pd
 import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
@@ -9,7 +10,8 @@ st.write("Choose the fruits you want in your custom Smoothie!")
 cnx = st. connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table(
-    "smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+    "smoothies.public.fruit_options").select(col('SEARCH_ON'))
+pd_df = my_dataframe.to_pandas()
 
 name_on_order = st.text_input("Choose your name:")
 st.write('The name on your Smoothie will be:', name_on_order)
@@ -22,6 +24,10 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(INGREDIENTS, NAME_ON_ORDER)
         values ('""" + ingredients_string + """','""" + name_on_order + """')"""
     for fruit_chosen in ingredients_list:
+        search_on = pd_df.loc[pd_df['FRUIT_NAME']
+                              == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get(
             "https://fruityvice.com/api/fruit/" + fruit_chosen)
