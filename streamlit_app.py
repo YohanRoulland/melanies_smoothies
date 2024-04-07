95f56463f0a895f6fdd850120430e8c2744ec0e9
Yohan+ <yohan.roulland@gmail.com>
2024-04-07 16:10:45 +0200

    correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.write("Choose the fruits you want in your custom Smoothie!")
 cnx = st. connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table(
-    "smoothies.public.fruit_options").select(col('SEARCH_ON'))
+    "smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 pd_df = my_dataframe.to_pandas()
 st.write(pd_df)
 
