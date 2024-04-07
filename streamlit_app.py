c41ae7e2658aee630e6b93f8d89b848c2add100f
Yohan+ <yohan.roulland@gmail.com>
2024-04-07 15:48:52 +0200

    info nutri

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,10 +22,9 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(INGREDIENTS, NAME_ON_ORDER)
         values ('""" + ingredients_string + """','""" + name_on_order + """')"""
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get(
-            "https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
+            "https://fruityvice.com/api/fruit/" + fruit_chosen)
 
         fv_df = st. dataframe(data=fruityvice_response.json(),
                               use_container_width=True)
